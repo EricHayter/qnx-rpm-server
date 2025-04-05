7ca117b5daad2475d17558e87ea421e3952674d2
Eric <hayter.eric@gmail.com>
2025-04-05 16:47:53 -0400

    Create authentication functions

--- src/auth.h
+++ src/auth.h
@@ -1,16 +1,32 @@
 #pragma once
 
 #include <string>
+#include <string_view>
+#include <optional>
+#include <filesystem>
 
-// These are the allowable character for crypt(). There are 64 in total
-// or 2^6.
-static const char * const VALID_CHARS = "0123456789"
-	"abcdefghijklmnopqrstuvwxyz"
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	"./";
+namespace Authenticator
+{
+	const std::filesystem::path LOGIN_FILE = "/etc/rpm_login";
+
+	// Specifies the privileges that users have
+	enum UserType {
+		VIEWER, // can view running processes
+		ADMIN, // can view and suspend running processes
+	};
+
+	struct UserEntry {
+		std::string username;
+		std::string hash;
+		std::string salt;
+		UserType type;	
+
+		static std::optional<UserEntry> FromString(std::string_view line);
+	};
 
 
-std::string generate_salt();
+	bool ValidateLogin(std::string_view username, std::string_view password);
+	std::string generate_hash(std::string_view password, std::string_view salt);
+	std::string generate_salt();
+}
 
-
-bool validate_password(char * const password, char * const salt, char * const hash);
